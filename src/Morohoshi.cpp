0846649f868078adefbe5c6cce799784e4ce14b5
MOROHOSHI TOMOYA <pandaaa0909@gmail.com>
2016-09-24 21:46:38 +0900

    add GUI

---
+++ src/Morohoshi.cpp
@@ -0,0 +1,17 @@
+//
+//  Morohoshi.cpp
+//  ProjectionMapping
+//
+//  Created by 諸星智也 on 2016/09/24.
+//
+//
+
+#include "Morohoshi.hpp"
+
+void Morohoshi::draw()
+{
+    //色を指定
+    ofSetColor(31, 127, 255);
+    //画面の中央に円を描く
+    ofCircle(ofGetWidth()/2, ofGetHeight()/2, 100);
+}
---
+++ src/Morohoshi.hpp
@@ -0,0 +1,18 @@
+//
+//  Morohoshi.hpp
+//  ProjectionMapping
+//
+//  Created by 諸星智也 on 2016/09/24.
+//
+//
+
+#include "ofMain.h"
+
+class Morohoshi {
+    
+public:
+    void PostGlitch();
+    
+private:
+    
+};
